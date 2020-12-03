d191cbd16acd9b5588a13c5e50e33af97f8587c1
anbreaker <antunez19@gmail.com>
2020-12-03 11:21:10 +0100

    retocando footer

--- src/components/layout/partials/FooterNav.js
+++ src/components/layout/partials/FooterNav.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const FooterNav = ({
-  className,
-  ...props
-}) => {
-
-  const classes = classNames(
-    'footer-nav',
-    className
-  );
+const FooterNav = ({className, ...props}) => {
+  const classes = classNames('footer-nav', className);
 
   return (
-    <nav
-      {...props}
-      className={classes}
-    >
+    <nav {...props} className={classes}>
       <ul className="list-reset">
         <li>
           <Link to="#0">Contact</Link>
@@ -25,14 +15,11 @@ const FooterNav = ({
           <Link to="#0">About us</Link>
         </li>
         <li>
-          <Link to="#0">FAQ's</Link>
-        </li>
-        <li>
           <Link to="#0">Support</Link>
         </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default FooterNav;
