e7e338b0fc5ee0caf6a35e9d1421d21dd399f9b5
anbreaker <antunez19@gmail.com>
2020-12-04 19:35:03 +0100

    agregando proyectos e info

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -65,14 +65,22 @@ const FeaturesSplit = ({
                 className="split-item-content center-content-mobile reveal-from-left"
                 data-reveal-container=".split-item">
                 <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
-                  Lightning fast workflow
+                  Deploy on AWS
                 </div>
                 <h3 className="mt-0 mb-12">Data-driven insights</h3>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua — Ut enim ad minim
-                  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                  commodo consequat.
+                  Nodejs and Mongodb Complete application (Login, Registration,CRUD, ES6+,
+                  and more)
+                </p>
+                <br />
+                <p className="m-0">
+                  This is a basic App to manage simple Notes on the web using Javascript
+                  Technologies like Nodejs, Mongodb, and so on.
+                </p>
+                <br />
+                <p className="m-0">
+                  Create, read, update, and delete Notes Allows a user to do log in and
+                  save his personal notes.
                 </p>
               </div>
               <div
--- src/components/sections/FeaturesTiles.js
+++ src/components/sections/FeaturesTiles.js
@@ -163,6 +163,33 @@ const FeaturesTiles = ({
               </div>
             </div>
 
+            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
+              <div className="tiles-item-inner">
+                <div className="features-tiles-item-header">
+                  <div className="features-tiles-item-image mb-16">
+                    <a
+                      href="https://github.com/anbreaker/webNodejsMongodb"
+                      target="_blank"
+                      rel="noreferrer noopener">
+                      <Image
+                        src={require('./../../assets/images/Orion_web.svg')}
+                        alt="Features tile icon 03"
+                        width={64}
+                        height={64}
+                      />
+                    </a>
+                  </div>
+                </div>
+                <div className="features-tiles-item-content">
+                  <h4 className="mt-0 mb-8">Nodejs y Mongodb</h4>
+                  <p className="m-0 text-sm">
+                    NodeJs and MongoDB CRUD Complete APP FullStack whitout frameworks JS,
+                    but use Bootstrap4. <b>(Login, Register,CRUD, ES6+, and more)</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+
             <div className="tiles-item reveal-from-bottom" data-reveal-delay="200">
               <div className="tiles-item-inner">
                 <div className="features-tiles-item-header">
@@ -185,28 +212,6 @@ const FeaturesTiles = ({
                   <p className="m-0 text-sm">
                     A littel exercise for work and learn, CRUD Complete APP FullStack
                     whitout frameworks JS, but use Bootstrap4.
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="tiles-item reveal-from-bottom" data-reveal-delay="400">
-              <div className="tiles-item-inner">
-                <div className="features-tiles-item-header">
-                  <div className="features-tiles-item-image mb-16">
-                    <Image
-                      src={require('./../../assets/images/feature-tile-icon-06.svg')}
-                      alt="Features tile icon 06"
-                      width={64}
-                      height={64}
-                    />
-                  </div>
-                </div>
-                <div className="features-tiles-item-content">
-                  <h4 className="mt-0 mb-8">Robust Workflow</h4>
-                  <p className="m-0 text-sm">
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                    dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.
                   </p>
                 </div>
               </div>
