56fb95b6dd07c16a1aacec3daad32737de92166a
anbreaker <antunez19@gmail.com>
2021-03-31 11:07:47 +0200

    fix iframe on mobile

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -113,13 +113,17 @@ const FeaturesSplit = ({
                     height: 0,
                   }}>
                   <iframe
-                    width="560"
-                    height="315"
-                    src="https://www.youtube.com/embed/NNaEmqyjiJE"
-                    title="YouTube video player"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
+                    title="unique"
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                      width: '100%',
+                      height: '100%',
+                    }}
+                    src={`https://www.youtube.com/embed/NNaEmqyjiJE`}
+                    frameBorder="0"
+                  />
                 </div>
               </div>
             </div>
